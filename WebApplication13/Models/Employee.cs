104de62150039ae072d1794226fe41b6e35f7872
tahanadeem11 <81690795+tahanadeem11@users.noreply.github.com>
2023-05-05 11:56:26 +0500

    Crud Done

--- WebApplication13/Models/Employee.cs
+++ WebApplication13/Models/Employee.cs
@@ -12,14 +12,10 @@ namespace WebApplication13.Models
     public class Employee
 
     {
-
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int id { get; set; }
-        
-
         public string name { get; set; }
-
         public string gender { get; set; }
-
         public string city { get; set; }
         public int DepartmentId { get; set; }
 
